d256e26d942ee9bfccdcfb05bc3410a9757d0289
Liam <git@liamsherwin.com>
2025-12-06 23:01:09 +1300

    Added WIP protocall documentation for Constellation

--- app/constellation/_meta.js
+++ app/constellation/_meta.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
 	"index": "Introduction",
-	// "guide": "Guide",
-	// "panels": "Panels",
-	// "components": "Components",
+	"datatypes": "Data Types",
+	"packets": "Packets",
 };
---
+++ app/constellation/packets/_meta.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	"Headder": "Headder",
+	"Discovery": "Discovery",
+	"Goodbye": "Goodbye",
+	"Command": "Command",
+	"MultiPart": "MultiPart",
+
+	"SetAttribute": "SetAttribute",
+	"StateRequest": "StateRequest",
+	"Heartbeat": "Heartbeat",
+
+	"SessionAnnounce": "SessionAnnounce",
+	"SessionDiscovery": "SessionDiscovery",
+	"SessionJoin": "SessionJoin",
+	"SessionLeave": "SessionLeave",
+	"SessionSetPriority": "SessionSetPriority",
+	"SessionSetMaster": "SessionSetMaster",
+
+	"SysExclusive": "SysExclusive"
+};
